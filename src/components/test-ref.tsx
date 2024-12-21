1c8321cc612a2029da6f0abb948a803bfe0c1681
techzealot <techzealot@foxmail.com>
2024-12-22 01:58:48 +0800

    add comment

--- src/components/test-ref.tsx
+++ src/components/test-ref.tsx
@@ -1,5 +1,7 @@
 import { ComponentPropsWithRef } from "react";
 
+// ComponentPropsWithRef与ComponentPropsWithoutRef区别是类型声明中是否包含ref属性
+// ComponentPropsWithRef与ComponentProps只有语义上的区别，并无实际区别
 const TestRef = ({ ref, ...props }: ComponentPropsWithRef<"button">) => {
   return (
     <button ref={ref} {...props}>
